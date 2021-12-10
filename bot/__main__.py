9bb06742601badc0948d567cf27e136b5e739a41
/ SL MaLiYa / <85741405+SL-Alpha-X@users.noreply.github.com>
2021-12-10 17:29:28 +0530

    `✨️ V.2 Released Now 🚀`

---
+++ bot/__main__.py
@@ -0,0 +1,24 @@
+# 🍁 Fast & High Quality YT Downloader Bot 🔥
+
+from pyrogram import Client
+import config
+
+DOWNLOAD_LOCATION = "./Downloads"
+BOT_TOKEN = config.BOT_TOKEN
+
+APP_ID = config.APP_ID
+API_HASH = config.API_HASH
+
+
+plugins = dict(
+    root="plugins",
+)
+
+Client(
+    "YouTubeDlBot",
+    bot_token=BOT_TOKEN,
+    api_id=APP_ID,
+    api_hash=API_HASH,
+    plugins=plugins,
+    workers=100
+).run()
